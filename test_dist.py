7e936ff7025a76f98126fe7c0a0666ece7e8ed3c
Abhinav Venigalla <abhi@mosaicml.com>
2022-06-15 22:57:08 +0000

    updates

--- test_dist.py
+++ test_dist.py
@@ -1,14 +1,15 @@
 import datetime
+import os
 import torch
 import torch.distributed as dist
 
 backend = 'nccl'
 dist.init_process_group(backend=backend, timeout=datetime.timedelta(seconds=30))
-dist.barrier()
 
-print (f"rank={dist.get_rank()}")
+local_rank = os.environ["LOCAL_RANK"]
 
 print ("test broadcast")
-a = torch.ones(3).cuda()
+a = torch.ones(3).to(f"cuda:{local_rank}")
 dist.broadcast(a, src=0)
+dist.barrier()
 print (a)
