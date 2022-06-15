ed9612fa97b4a70179d521544ab508aeec0eecba
Abhinav Venigalla <abhi@mosaicml.com>
2022-06-15 23:01:49 +0000

    updates2

--- test_dist.py
+++ test_dist.py
@@ -8,8 +8,13 @@ dist.init_process_group(backend=backend, timeout=datetime.timedelta(seconds=30))
 
 local_rank = os.environ["LOCAL_RANK"]
 
-print ("test broadcast")
+print ("test all-reduce")
 a = torch.ones(3).to(f"cuda:{local_rank}")
+dist.all_reduce(a)
+print (a)
+
+
+print ("test broadcast")
 dist.broadcast(a, src=0)
 dist.barrier()
 print (a)
